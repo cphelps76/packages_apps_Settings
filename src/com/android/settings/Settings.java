78de4916d6891df46cdacd3918ec6dabbe1d9a54
Bajee11 <bajee11@gmail.com>
2013-03-01 08:03:27 -0500

    QuietHours Toggle (2/2)
    Required for longpress action.
    
    Reflect change in Settings.
    
    Signed-off-by: Bajee11 <bajee11@gmail.com>
    Change-Id: I2082e12fb2ce9e4063d960df540259eb6822fbcd
    
    Conflicts:
    	src/com/android/settings/Settings.java
    	src/com/android/settings/cyanogenmod/QuietHours.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -871,4 +871,5 @@ public class Settings extends PreferenceActivity
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
     public static class DreamSettingsActivity extends Settings { /* empty */ }
     public static class NotificationShortcutsSettingsActivity extends Settings { /* empty */ }
+    public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
 }
