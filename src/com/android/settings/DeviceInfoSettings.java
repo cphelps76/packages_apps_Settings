6697376b9061d3616f63cf984ef240992a13528d
cphelps76 <cphelps76@gmail.com>
2013-07-21 21:26:36 -0500

    add kernel toolchain to rom's about screen
    Dependant on https://github.com/cphelps76/android_build/commit/57c340e8d66e2d21716faffb930062861bbc11bd

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -55,6 +55,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
     private static final String PROPERTY_URL_SAFETYLEGAL = "ro.url.safetylegal";
     private static final String PROPERTY_SELINUX_STATUS = "ro.build.selinux";
     private static final String KEY_KERNEL_VERSION = "kernel_version";
+    private static final String KEY_KERNEL_TOOLCHAIN = "kernel_toolchain";
     private static final String KEY_BUILD_NUMBER = "build_number";
     private static final String KEY_DEVICE_MODEL = "device_model";
     private static final String KEY_SELINUX_STATUS = "selinux_status";
@@ -88,6 +89,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
+        setValueSummary(KEY_KERNEL_TOOLCHAIN, "ro.kernel.toolchain");
         setValueSummary(KEY_MOD_VERSION, "ro.modversion");
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
         setValueSummary(KEY_MOD_BUILD_HOST, "ro.build.user");
