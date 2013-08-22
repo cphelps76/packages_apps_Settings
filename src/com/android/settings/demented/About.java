3447663c58921329becb71ac4f99fd1d9a9a0e96
cphelps76 <cphelps76@gmail.com>
2013-08-22 12:10:18 -0500

    add About screen to DEMENTED

---
+++ src/com/android/settings/demented/About.java
@@ -0,0 +1,47 @@
+package com.android.settings.demented;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class About extends SettingsPreferenceFragment {
+
+    public static final String TAG = "About";
+
+    Preference mSiteUrl;
+    Preference mSourceUrl;
+    Preference mSourceDCUrl;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.prefs_about);
+        mSiteUrl = findPreference("dc_website");
+        mSourceUrl = findPreference("demented_source");
+        mSourceDCUrl = findPreference("dc_source");
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mSiteUrl) {
+            launchUrl("http://www.forums.droidconcepts.com/");
+        } else if (preference == mSourceUrl) {
+            launchUrl("https://github.com/cphelps76");
+        } else if (preference == mSourceDCUrl) {
+            launchUrl("https://github.com/davros-");
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void launchUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent donate = new Intent(Intent.ACTION_VIEW, uriUrl);
+        getActivity().startActivity(donate);
+    }
+}
