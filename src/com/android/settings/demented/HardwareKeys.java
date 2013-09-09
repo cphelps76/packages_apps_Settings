25a7fb23b97ba22c91f303aa9f7ce7562f37e56f
cphelps76 <cphelps76@gmail.com>
2013-09-08 22:59:38 -0500

    Settings: Add torch toggle to hardware key rebinding (2/2)

--- src/com/android/settings/demented/HardwareKeys.java
+++ src/com/android/settings/demented/HardwareKeys.java
@@ -68,6 +68,7 @@ public class HardwareKeys extends SettingsPreferenceFragment implements
     private static final int ACTION_KILL_APP = 11;
     private static final int ACTION_LAST_APP = 12;
     private static final int ACTION_CUSTOM_APP = 13;
+    private static final int ACTION_TORCH = 14;
 
     // Masks for checking presence of hardware keys.
     // Must match values in frameworks/base/core/res/res/values/config.xml
