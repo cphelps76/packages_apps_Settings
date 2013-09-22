willl0344/android_packages_apps_Settings

First pass at generic hardware tunables support
With support for vibrator intensity and kcal

Change-Id: Ice3444b31a9ab0a25cf25bebb18351e2c08a70c3

src/com/android/settings/DisplaySettings.java
@@ -44,6 +44,7 @@ import android.provider.Settings;
 import android.util.Log;
 
 import com.android.internal.view.RotationPolicy;
+import com.android.settings.cyanogenmod.DisplayColor;
 import com.android.settings.cyanogenmod.DisplayRotation;
 import com.android.settings.Utils;
 
@@ -66,6 +67,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_HOME_WAKE = "pref_home_wake";
     private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
     private static final String KEY_SCREEN_OFF_ANIMATION = "screen_off_animation";
+    private static final String KEY_DISPLAY_COLOR = "color_calibration";
 
     // Strings used for building the summary
     private static final String ROTATION_ANGLE_0 = "0";
@@ -196,6 +198,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     Settings.System.SCREEN_OFF_ANIMATION, 1) == 1);
         } else {
             getPreferenceScreen().removePreference(mScreenOffAnimation);
+        }
+
+        if (!DisplayColor.isSupported()) {
+            removePreference(KEY_DISPLAY_COLOR);
         }
     }
 

src/com/android/settings/SoundSettings.java
@@ -54,6 +54,8 @@ import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.VolumePanel;
 
+import com.android.settings.cyanogenmod.VibratorIntensity;
+
 import java.util.Date;
 import java.util.Calendar;
 import java.util.List;
@@ -96,6 +98,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private static final String KEY_POWER_NOTIFICATIONS = "power_notifications";
     private static final String KEY_POWER_NOTIFICATIONS_VIBRATE = "power_notifications_vibrate";
     private static final String KEY_POWER_NOTIFICATIONS_RINGTONE = "power_notifications_ringtone";
+    private static final String KEY_VIBRATOR_INTENSITY = "vibrator_intensity";
 
     private static final String RING_MODE_NORMAL = "normal";
     private static final String RING_MODE_VIBRATE = "vibrate";
@@ -349,6 +352,11 @@ public class SoundSettings extends SettingsPreferenceFragment implements
                 Settings.Global.POWER_NOTIFICATIONS_VIBRATE, 0) != 0);
         if (vibrator == null || !vibrator.hasVibrator()) {
             removePreference(KEY_POWER_NOTIFICATIONS_VIBRATE);
+            removePreference(KEY_VIBRATOR_INTENSITY);
+        }
+
+        if (!VibratorIntensity.isSupported()) {
+            removePreference(KEY_VIBRATOR_INTENSITY);
         }
 
         mPowerSoundsRingtone = findPreference(KEY_POWER_NOTIFICATIONS_RINGTONE);

src/com/android/settings/cyanogenmod/DisplayColor.java
@@ -1,0 +1,188 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.preference.DialogPreference;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.util.Log;
+import android.widget.Button;
+
+import com.android.settings.R;
+
+import org.cyanogenmod.hardware.DisplayColorCalibration;
+
+/**
+ * Special preference type that allows configuration of Color settings
+ */
+public class DisplayColor extends DialogPreference {
+    private static final String TAG = "ColorCalibration";
+
+    // These arrays must all match in length and order
+    private static final int[] SEEKBAR_ID = new int[] {
+        R.id.color_red_seekbar,
+        R.id.color_green_seekbar,
+        R.id.color_blue_seekbar
+    };
+
+    private static final int[] SEEKBAR_VALUE_ID = new int[] {
+        R.id.color_red_value,
+        R.id.color_green_value,
+        R.id.color_blue_value
+    };
+
+    private ColorSeekBar[] mSeekBars = new ColorSeekBar[SEEKBAR_ID.length];
+    private String[] mCurrentColors;
+    private String mOriginalColors;
+
+    public DisplayColor(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        if (!isSupported()) {
+            return;
+        }
+
+        setDialogLayoutResource(R.layout.display_color_calibration);
+    }
+
+    @Override
+    protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
+        builder.setNeutralButton(R.string.auto_brightness_reset_button,
+                new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+
+        mOriginalColors = DisplayColorCalibration.getCurColors();
+        mCurrentColors = mOriginalColors.split(" ");
+
+        for (int i = 0; i < SEEKBAR_ID.length; i++) {
+            SeekBar seekBar = (SeekBar) view.findViewById(SEEKBAR_ID[i]);
+            TextView value = (TextView) view.findViewById(SEEKBAR_VALUE_ID[i]);
+            mSeekBars[i] = new ColorSeekBar(seekBar, value, i, Integer.valueOf(mCurrentColors[i]));
+        }
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+
+        // Can't use onPrepareDialogBuilder for this as we want the dialog
+        // to be kept open on click
+        AlertDialog d = (AlertDialog) getDialog();
+        Button defaultsButton = d.getButton(DialogInterface.BUTTON_NEUTRAL);
+        defaultsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (ColorSeekBar csb : mSeekBars) {
+                    csb.mSeekBar.setProgress(DisplayColorCalibration.getMaxValue());
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+
+        if (positiveResult) {
+            Editor editor = getEditor();
+            editor.putString("display_color_calibration", DisplayColorCalibration.getCurColors());
+            editor.commit();
+        } else {
+            DisplayColorCalibration.setColors(mOriginalColors);
+        }
+    }
+
+    public static boolean isSupported() {
+        try {
+            return DisplayColorCalibration.isSupported();
+        } catch (NoClassDefFoundError e) {
+            // Hardware abstraction framework isn't installed
+            return false;
+        }
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported()) {
+            return;
+        }
+
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        final String value = prefs.getString("display_color_calibration", null);
+
+        if (value != null) {
+            Log.d(TAG, "Restoring color values: " + value);
+            DisplayColorCalibration.setColors(value);
+        }
+    }
+
+    private class ColorSeekBar implements SeekBar.OnSeekBarChangeListener {
+        private int mIndex;
+        private int mOriginal;
+        private SeekBar mSeekBar;
+        private TextView mValue;
+
+        public ColorSeekBar(SeekBar seekBar, TextView value, int index, int original) {
+            mSeekBar = seekBar;
+            mValue = value;
+            mIndex = index;
+            mOriginal = original;
+
+            mSeekBar.setMax(DisplayColorCalibration.getMaxValue());
+            mSeekBar.setOnSeekBarChangeListener(this);
+            mSeekBar.setProgress(mOriginal);
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            mCurrentColors[mIndex] = String.valueOf(progress);
+            DisplayColorCalibration.setColors(TextUtils.join(" ", mCurrentColors));
+
+            int min = DisplayColorCalibration.getMinValue();
+            int max = DisplayColorCalibration.getMaxValue();
+            int percent = Math.round(100F * progress / (max - min));
+            mValue.setText(String.format("%d%%", percent));
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            // Do nothing here
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // Do nothing here
+        }
+    }
+}

cphelps76/packages_apps_Settings

Conflicts:
	AndroidManifest.xml
	res/values/cm_strings.xml
	res/xml/display_settings.xml
	res/xml/sound_settings.xml
	src/com/android/settings/DisplaySettings.java
	src/com/android/settings/SoundSettings.java

src/com/android/settings/DisplaySettings.java
@@ -43,6 +43,7 @@ import android.provider.Settings;
 import android.util.Log;
 
 import com.android.internal.view.RotationPolicy;
+import com.android.settings.demented.DisplayColor;
 import com.android.settings.demented.DisplayRotation;
 import com.android.settings.Utils;
 
@@ -60,6 +61,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_SCREEN_SAVER = "screensaver";
     private static final String KEY_WIFI_DISPLAY = "wifi_display";
     private static final String KEY_DISPLAY_ROTATION = "display_rotation";
+    private static final String KEY_DISPLAY_COLOR = "color_calibration";
 
     // Strings used for building the summary
     private static final String ROTATION_ANGLE_0 = "0";
@@ -143,6 +145,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 == WifiDisplayStatus.FEATURE_STATE_UNAVAILABLE) {
             getPreferenceScreen().removePreference(mWifiDisplayPreference);
             mWifiDisplayPreference = null;
+        }
+
+        if (!DisplayColor.isSupported()) {
+            removePreference(KEY_DISPLAY_COLOR);
         }
     }
 

src/com/android/settings/SoundSettings.java
@@ -50,6 +50,8 @@ import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.VolumePanel;
 
+import com.android.settings.demented.VibratorIntensity;
+
 import java.util.Date;
 import java.util.Calendar;
 import java.util.List;
@@ -83,11 +85,10 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private static final String KEY_DOCK_SOUNDS = "dock_sounds";
     private static final String KEY_DOCK_AUDIO_MEDIA_ENABLED = "dock_audio_media_enabled";
     private static final String KEY_QUIET_HOURS = "quiet_hours";
-
-
     private static final String SILENT_MODE_OFF = "off";
     private static final String SILENT_MODE_VIBRATE = "vibrate";
     private static final String SILENT_MODE_MUTE = "mute";
+    private static final String KEY_VIBRATOR_INTENSITY = "vibrator_intensity";
 
     private static final String[] NEED_VOICE_CAPABILITY = {
             KEY_RINGTONE, KEY_DTMF_TONE, KEY_CATEGORY_CALLS,
@@ -203,6 +204,11 @@ public class SoundSettings extends SettingsPreferenceFragment implements
         if (vibrator == null || !vibrator.hasVibrator()) {
             removePreference(KEY_VIBRATE);
             removePreference(KEY_HAPTIC_FEEDBACK);
+            removePreference(KEY_VIBRATOR_INTENSITY);
+        }
+
+        if (!VibratorIntensity.isSupported()) {
+            removePreference(KEY_VIBRATOR_INTENSITY);
         }
 
         if (TelephonyManager.PHONE_TYPE_CDMA == activePhoneType) {
@@ -529,4 +535,3 @@ public class SoundSettings extends SettingsPreferenceFragment implements
         return ab.create();
     }
 }
-

src/com/android/settings/demented/DisplayColor.java
@@ -1,0 +1,188 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.demented;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.preference.DialogPreference;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.util.Log;
+import android.widget.Button;
+
+import com.android.settings.R;
+
+import org.cyanogenmod.hardware.DisplayColorCalibration;
+
+/**
+ * Special preference type that allows configuration of Color settings
+ */
+public class DisplayColor extends DialogPreference {
+    private static final String TAG = "ColorCalibration";
+
+    // These arrays must all match in length and order
+    private static final int[] SEEKBAR_ID = new int[] {
+        R.id.color_red_seekbar,
+        R.id.color_green_seekbar,
+        R.id.color_blue_seekbar
+    };
+
+    private static final int[] SEEKBAR_VALUE_ID = new int[] {
+        R.id.color_red_value,
+        R.id.color_green_value,
+        R.id.color_blue_value
+    };
+
+    private ColorSeekBar[] mSeekBars = new ColorSeekBar[SEEKBAR_ID.length];
+    private String[] mCurrentColors;
+    private String mOriginalColors;
+
+    public DisplayColor(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        if (!isSupported()) {
+            return;
+        }
+
+        setDialogLayoutResource(R.layout.display_color_calibration);
+    }
+
+    @Override
+    protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
+        builder.setNeutralButton(R.string.reset_button,
+                new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+
+        mOriginalColors = DisplayColorCalibration.getCurColors();
+        mCurrentColors = mOriginalColors.split(" ");
+
+        for (int i = 0; i < SEEKBAR_ID.length; i++) {
+            SeekBar seekBar = (SeekBar) view.findViewById(SEEKBAR_ID[i]);
+            TextView value = (TextView) view.findViewById(SEEKBAR_VALUE_ID[i]);
+            mSeekBars[i] = new ColorSeekBar(seekBar, value, i, Integer.valueOf(mCurrentColors[i]));
+        }
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+
+        // Can't use onPrepareDialogBuilder for this as we want the dialog
+        // to be kept open on click
+        AlertDialog d = (AlertDialog) getDialog();
+        Button defaultsButton = d.getButton(DialogInterface.BUTTON_NEUTRAL);
+        defaultsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (ColorSeekBar csb : mSeekBars) {
+                    csb.mSeekBar.setProgress(DisplayColorCalibration.getMaxValue());
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+
+        if (positiveResult) {
+            Editor editor = getEditor();
+            editor.putString("display_color_calibration", DisplayColorCalibration.getCurColors());
+            editor.commit();
+        } else {
+            DisplayColorCalibration.setColors(mOriginalColors);
+        }
+    }
+
+    public static boolean isSupported() {
+        try {
+            return DisplayColorCalibration.isSupported();
+        } catch (NoClassDefFoundError e) {
+            // Hardware abstraction framework isn't installed
+            return false;
+        }
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported()) {
+            return;
+        }
+
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        final String value = prefs.getString("display_color_calibration", null);
+
+        if (value != null) {
+            Log.d(TAG, "Restoring color values: " + value);
+            DisplayColorCalibration.setColors(value);
+        }
+    }
+
+    private class ColorSeekBar implements SeekBar.OnSeekBarChangeListener {
+        private int mIndex;
+        private int mOriginal;
+        private SeekBar mSeekBar;
+        private TextView mValue;
+
+        public ColorSeekBar(SeekBar seekBar, TextView value, int index, int original) {
+            mSeekBar = seekBar;
+            mValue = value;
+            mIndex = index;
+            mOriginal = original;
+
+            mSeekBar.setMax(DisplayColorCalibration.getMaxValue());
+            mSeekBar.setOnSeekBarChangeListener(this);
+            mSeekBar.setProgress(mOriginal);
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            mCurrentColors[mIndex] = String.valueOf(progress);
+            DisplayColorCalibration.setColors(TextUtils.join(" ", mCurrentColors));
+
+            int min = DisplayColorCalibration.getMinValue();
+            int max = DisplayColorCalibration.getMaxValue();
+            int percent = Math.round(100F * progress / (max - min));
+            mValue.setText(String.format("%d%%", percent));
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            // Do nothing here
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // Do nothing here
+        }
+    }
+}