fc3af4b1e4a1553fa7880c6873226ddd0ec3e359
GalaticStryder <icarohoff@gmail.com>
2016-02-29 15:53:20 -0300

    Settings - DEMENTED logo on status bar (2/2)
    Change-Id: Ic2c1f8cb6800cc4c1a45bfcabc1162d48385a065
    Signed-off-by: Cphelps76 <cphelps76@gmail.com>

--- src/com/android/settings/demented/StatusBarSettings.java
+++ src/com/android/settings/demented/StatusBarSettings.java
@@ -44,6 +44,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
 import cyanogenmod.providers.CMSettings;
 
 public class StatusBarSettings extends SettingsPreferenceFragment
@@ -51,6 +53,8 @@ public class StatusBarSettings extends SettingsPreferenceFragment
 
     private static final String TAG = "StatusBar";
 
+    private static final String KEY_DEMENTED_LOGO = "status_bar_demented_logo";
+    private static final String KEY_DEMENTED_LOGO_COLOR = "status_bar_demented_logo_color";
     private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
     private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
@@ -62,6 +66,8 @@ public class StatusBarSettings extends SettingsPreferenceFragment
 
     private static final String KEY_SHOW_4G = "show_4G_for_LTE";
 
+    private SwitchPreference mDEMENTEDLogo;
+    private ColorPickerPreference mDEMENTEDLogoColor;
     private ListPreference mStatusBarClock;
     private ListPreference mStatusBarAmPm;
     private ListPreference mStatusBarBattery;
@@ -83,6 +89,9 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         mStatusBarBatteryShowPercent =
                 (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
         mQuickPulldown = (ListPreference) findPreference(STATUS_BAR_QUICK_QS_PULLDOWN);
+        mShow4g = (SwitchPreference) findPreference(KEY_SHOW_4G);
+        mDEMENTEDLogo = (SwitchPreference) findPreference(KEY_DEMENTED_LOGO);
+        mDEMENTEDLogoColor = (ColorPickerPreference) findPreference(KEY_DEMENTED_LOGO_COLOR);
 
         int clockStyle = CMSettings.System.getInt(resolver,
                 CMSettings.System.STATUS_BAR_CLOCK, 1);
@@ -114,7 +123,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         enableStatusBarBatteryDependents(batteryStyle);
         mStatusBarBatteryShowPercent.setOnPreferenceChangeListener(this);
 
-        mShow4g = (SwitchPreference) findPreference(KEY_SHOW_4G);
         mShow4g.setChecked(Settings.System.getInt(resolver,
                 Settings.System.SHOW_4G_FOR_LTE, 0) != 0);
 
@@ -123,6 +131,17 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         mQuickPulldown.setValue(String.valueOf(quickPulldown));
         updatePulldownSummary(quickPulldown);
         mQuickPulldown.setOnPreferenceChangeListener(this);
+
+        mDEMENTEDLogo.setChecked(Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_DEMENTED_LOGO, 0) != 0);
+        enableLogoColorPref();
+
+        mDEMENTEDLogoColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_DEMENTED_LOGO_COLOR, 0xffffffff);
+        String hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mDEMENTEDLogoColor.setSummary(hexColor);
+        mDEMENTEDLogoColor.setNewPreviewColor(intColor);
 
         isTelephony();
     }
@@ -152,12 +171,25 @@ public class StatusBarSettings extends SettingsPreferenceFragment
                         R.array.status_bar_clock_style_entries_rtl));
                 mStatusBarClock.setSummary(mStatusBarClock.getEntry());
         }
+        enableLogoColorPref();
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mStatusBarClock) {
+
+        if (preference == mDEMENTEDLogo) {
+            enableLogoColorPref();
+            return true;
+        } else if (preference == mDEMENTEDLogoColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(resolver,
+                    Settings.System.STATUS_BAR_DEMENTED_LOGO_COLOR, intHex);
+            return true;
+        } else if (preference == mStatusBarClock) {
             int clockStyle = Integer.parseInt((String) newValue);
             int index = mStatusBarClock.findIndexOfValue((String) newValue);
             CMSettings.System.putInt(
@@ -199,14 +231,31 @@ public class StatusBarSettings extends SettingsPreferenceFragment
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mShow4g) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.SHOW_4G_FOR_LTE,
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        if (preference == mDEMENTEDLogo) {
+            Settings.System.putInt(resolver, Settings.System.STATUS_BAR_DEMENTED_LOGO,
+                    mDEMENTEDLogo.isChecked() ? 1 : 0);
+        } else if (preference == mShow4g) {
+            Settings.System.putInt(resolver, Settings.System.SHOW_4G_FOR_LTE,
                     mShow4g.isChecked() ? 1 : 0);
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
         return true;
+    }
+
+    private boolean isDementedLogoEnabled() {
+        return Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_DEMENTED_LOGO, 0) != 0;
+    }
+
+    private void enableLogoColorPref() {
+        if (!isDementedLogoEnabled()) {
+            mDEMENTEDLogoColor.setEnabled(false);
+        } else {
+            mDEMENTEDLogoColor.setEnabled(true);
+        }
     }
 
     private void enableStatusBarBatteryDependents(int batteryIconStyle) {
