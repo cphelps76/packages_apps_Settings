c2a156a635c451d42610c01867fd714c873faf17
cphelps76 <cphelps76@gmail.com>
2016-06-28 16:22:06 -0500

    base: reconfigure Preference (2/2)   ...{x} allow for enabling/disabling custom styles

--- src/com/android/settings/demented/PreferenceStyle.java
+++ src/com/android/settings/demented/PreferenceStyle.java
@@ -27,17 +27,15 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.ContentObserver;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Handler;
 import android.net.Uri;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
@@ -54,11 +52,13 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 import java.lang.CharSequence;
+import java.lang.Integer;
 
 public class PreferenceStyle extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "PreferenceStyle";
+    private static final String KEY_ENABLE_STYLES = "enable_custom_text";
     private static final String KEY_TITLE_STYLE = "title_style";
     private static final String KEY_TITLE_COLOR_PICKER = "prefs_title_text_color";
     private static final String KEY_TITLE_TEXT_STYLE = "prefs_title_text_style";
@@ -66,15 +66,13 @@ public class PreferenceStyle extends SettingsPreferenceFragment implements
     private Activity mActivity;
     private static Context mContext;
     private ContentResolver mResolver;
-    private Handler mHandler;
-    private SettingsObserver mObserver;
     private PreferenceScreen mPrefScreen;
 
-    private PreferenceCategory mTitleOptions;
+    private SwitchPreference mEnableOptions;
     private ColorPickerPreference mTitleTextColor;
     private ListPreference mTitleTextStyle;
     private int mIntColor;
-    private int mDefaultColor;
+    private int mDefault = Integer.MIN_VALUE;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -85,29 +83,94 @@ public class PreferenceStyle extends SettingsPreferenceFragment implements
 
         mActivity = getActivity();
         mContext = getContext();
-        mHandler = new Handler();
         mPrefScreen = getPreferenceScreen();
         mResolver = getContentResolver();
 
-        mObserver = new SettingsObserver(mContext, mHandler);
-        mObserver.observe();
-
-        mTitleOptions = (PreferenceCategory) mPrefScreen.findPreference(KEY_TITLE_STYLE);
+        mEnableOptions = (SwitchPreference) mPrefScreen.findPreference(KEY_ENABLE_STYLES);
+        mEnableOptions.setTitle(R.string.title_style_label);
+        mEnableOptions.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.CUSTOM_TEXT_STYLE_ENABLED, 0) != 0);
 
         mTitleTextColor = (ColorPickerPreference) mPrefScreen.findPreference(KEY_TITLE_COLOR_PICKER);
         mTitleTextColor.setOnPreferenceChangeListener(this);
+
         mIntColor = Settings.System.getInt(mResolver,
                     Settings.System.SYSTEM_PREF_TEXT_COLOR, -2);
-        setTextColorSummary();
 
         mTitleTextStyle = (ListPreference) mPrefScreen.findPreference(KEY_TITLE_TEXT_STYLE);
         mTitleTextStyle.setOnPreferenceChangeListener(this);
         mTitleTextStyle.setValue(Integer.toString(Settings.System.getInt(mResolver,
                 Settings.System.SYSTEM_PREF_TEXT_STYLE, 0)));
-        mTitleTextStyle.setSummary(mTitleTextStyle.getEntry());
 
         Settings.System.putBoolean(mResolver,
                 Settings.System.SYSTEM_PREF_STYLE_CHANGED, false);
+
+        setSummaries();
+    }
+
+    private void setSummaries() {
+        setEnableOptionsSummary();
+        setTextColorSummary();
+        setTextStyleSummary();
+    }
+
+    private void setEnableOptionsSummary() {
+        boolean enabled = Settings.System.getInt(mResolver,
+                Settings.System.CUSTOM_TEXT_STYLE_ENABLED, 0) == 1;
+        if (!enabled) {
+            mEnableOptions.setSummary(R.string.custom_text_styles_disabled);
+        } else {
+            mEnableOptions.setSummary(R.string.custom_text_styles_enabled);
+        }
+    }
+
+    private void setTextColorSummary() {
+        int defaultColor = -1;
+        mIntColor = Settings.System.getInt(mResolver,
+                    Settings.System.SYSTEM_PREF_TEXT_COLOR, -2);
+        boolean enabled = Settings.System.getInt(mResolver,
+                Settings.System.CUSTOM_TEXT_STYLE_ENABLED, 0) == 1;
+        boolean isReset = Settings.System.getInt(mResolver,
+                Settings.System.SYSTEM_PREF_RESET, 0) == 1;
+        Log.i(TAG, "enabled is : " + enabled);
+        Log.i(TAG, "isReset is : " + isReset);
+        if (enabled) {
+            if (!isReset) {
+                if (mIntColor != mDefault) {
+                    String hexColor = String.format("#%08x", (0xffffffff & mIntColor));
+                    mTitleTextColor.setSummary(hexColor);
+                    mTitleTextColor.setNewPreviewColor(mIntColor);
+                    return;
+                }
+            } else {
+                mTitleTextColor.setSummary(R.string.text_color_default);
+                mTitleTextColor.setNewPreviewColor(defaultColor);
+                return;
+            }
+        } else {
+            mTitleTextColor.setSummary(R.string.text_color_default);
+            mTitleTextColor.setNewPreviewColor(defaultColor);
+        }
+    }
+
+    private void setTextStyleSummary() {
+        int currentFontStyle = Settings.System.getInt(mResolver,
+                Settings.System.SYSTEM_PREF_TEXT_STYLE, 0);
+        boolean enabled = Settings.System.getInt(mResolver,
+                Settings.System.CUSTOM_TEXT_STYLE_ENABLED, 0) == 1;
+        if (enabled) {
+            if (currentFontStyle == 0) {
+                mTitleTextStyle.setSummary(R.string.pref_text_style_normal);
+            } else if (currentFontStyle == 1) {
+                mTitleTextStyle.setSummary(R.string.pref_text_style_bold);
+            } else if (currentFontStyle == 2) {
+                mTitleTextStyle.setSummary(R.string.pref_text_style_italic);
+            } else if (currentFontStyle == 3) {
+                mTitleTextStyle.setSummary(R.string.pref_text_style_bold_italic);
+            }
+        } else {
+            mTitleTextStyle.setSummary(R.string.pref_text_style_normal);
+        }
     }
 
     private void setReset(int i) {
@@ -116,67 +179,49 @@ public class PreferenceStyle extends SettingsPreferenceFragment implements
         if (i == 1) {
             reset();
         }
-        setTextColorSummary();
-    }
-
-    private void setTextColorSummary() {
-        mIntColor = Settings.System.getInt(mResolver,
-                    Settings.System.SYSTEM_PREF_TEXT_COLOR, -2);
-        if (inversionModeEnabled()) {
-            mDefaultColor = getResources().getColor(
-                    com.android.internal.R.color.primary_text_default_material_light);
-        } else {
-            mDefaultColor = getResources().getColor(
-                    com.android.internal.R.color.primary_text_default_material_dark);
-        }
-        if (!isReset()) {
-            String hexColor = String.format("#%08x", (0xffffffff & mIntColor));
-            mTitleTextColor.setSummary(hexColor);
-        } else {
-            mIntColor = mDefaultColor;
-            mTitleTextColor.setSummary(R.string.text_color_default);
-        }
-        mTitleTextColor.setNewPreviewColor(mIntColor);
-    }
-
-    private boolean inversionModeEnabled() {
-        return Settings.Secure.getInt(mResolver,
-                Settings.Secure.ACCESSIBILITY_DISPLAY_INVERSION_ENABLED, 0) !=0;
-    }
-
-    private boolean isReset() {
-        return Settings.System.getInt(mResolver,
-                Settings.System.SYSTEM_PREF_RESET, 0) == 1;
     }
 
     private void reset() {
         Settings.System.putInt(mResolver,
-                Settings.System.SYSTEM_PREF_TEXT_COLOR, -2);
-        mTitleTextStyle.setValue(Integer.toString(Settings.System.getInt(mResolver,
-                Settings.System.SYSTEM_PREF_TEXT_STYLE, 0)));
+                Settings.System.CUSTOM_TEXT_STYLE_ENABLED, 0);
+        mEnableOptions.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.CUSTOM_TEXT_STYLE_ENABLED, 0) != 0);
+        Settings.System.putInt(mResolver,
+                Settings.System.SYSTEM_PREF_TEXT_COLOR, mDefault);
         Settings.System.putInt(mResolver,
                 Settings.System.SYSTEM_PREF_TEXT_STYLE, 0);
         mTitleTextStyle.setValue(Integer.toString(Settings.System.getInt(mResolver,
                 Settings.System.SYSTEM_PREF_TEXT_STYLE, 0)));
-        mTitleTextStyle.setSummary(mTitleTextStyle.getEntry());
-        setTextColorSummary();
+        reloadFragment();
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        final String key = preference.getKey();
+        boolean enabled = Settings.System.getInt(mResolver,
+                Settings.System.CUSTOM_TEXT_STYLE_ENABLED, 0) == 1;
         if (preference == mTitleTextColor) {
-            String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
-                    .valueOf(newValue)));
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(mResolver,
-                    Settings.System.SYSTEM_PREF_TEXT_COLOR, intHex);
+            if (enabled) {
+                String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
+                        .valueOf(newValue)));
+                int intHex = ColorPickerPreference.convertToColorInt(hex);
+                final int currentHex = Settings.System.getInt(mResolver,
+                        Settings.System.SYSTEM_PREF_TEXT_COLOR, -2);
+                if (intHex != currentHex) {
+                    Settings.System.putInt(mResolver,
+                            Settings.System.SYSTEM_PREF_TEXT_COLOR, intHex);
+                    reloadFragment();
+                }
+            }
             return true;
         } else if (preference == mTitleTextStyle) {
+            final int currentStyle = Settings.System.getInt(mResolver,
+                    Settings.System.SYSTEM_PREF_TEXT_STYLE, 0);
             int val = Integer.parseInt((String) newValue);
             int index = mTitleTextStyle.findIndexOfValue((String) newValue);
-            Settings.System.putInt(mResolver,
-                    Settings.System.SYSTEM_PREF_TEXT_STYLE, val);
-            mTitleTextStyle.setSummary(mTitleTextStyle.getEntries()[index]);
+            if (enabled && currentStyle != val) {
+                Settings.System.putInt(mResolver,
+                        Settings.System.SYSTEM_PREF_TEXT_STYLE, val);
+                reloadFragment();
+            }
             return true;
         }
         return false;
@@ -184,7 +229,16 @@ public class PreferenceStyle extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        setReset(0);
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mEnableOptions) {
+            Settings.System.putInt(resolver, Settings.System.CUSTOM_TEXT_STYLE_ENABLED,
+                    mEnableOptions.isChecked() ? 1 : 0);
+            reloadFragment();
+        } else if (preference == mTitleTextColor) {
+            setReset(0);
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
         return false;
     }
 
@@ -221,55 +275,22 @@ public class PreferenceStyle extends SettingsPreferenceFragment implements
                 .show();
     }
 
-    private final class SettingsObserver extends ContentObserver {
-        private boolean mRegistered;
-
-        private final Uri PREF_TEXT_COLOR =
-                Settings.System.getUriFor(Settings.System.SYSTEM_PREF_TEXT_COLOR);
-        private final Uri PREF_TEXT_STYLE =
-                Settings.System.getUriFor(Settings.System.SYSTEM_PREF_TEXT_STYLE);
-        private final Uri PREF_TEXT_RESET =
-                Settings.System.getUriFor(Settings.System.SYSTEM_PREF_RESET);
-
-        public SettingsObserver(Context context, Handler handler) {
-            super(handler);
-        }
-
-        public void observe() {
-            if (mRegistered) {
-                mResolver.unregisterContentObserver(this);
-            }
-            mResolver.registerContentObserver(PREF_TEXT_COLOR, false, this);
-            mResolver.registerContentObserver(PREF_TEXT_STYLE, false, this);
-            mResolver.registerContentObserver(PREF_TEXT_RESET, false, this);
-            mRegistered = true;
-        }
-
-        @Override
-        public void onChange(boolean selfChange, Uri uri) {
-            updateSettings();
-        }
-
-        private void updateSettings() {
-            Settings.System.putBoolean(mResolver,
-                    Settings.System.SYSTEM_PREF_STYLE_CHANGED, true);
-            reLoadFragment();
-        }
-    }
-
-    private void reLoadFragment() {
+    private void reloadFragment() {
+        Settings.System.putBoolean(mResolver,
+                Settings.System.SYSTEM_PREF_STYLE_CHANGED, true);
         try {
             FragmentManager manager = getFragmentManager();
             Fragment currentFragment = manager.findFragmentById(R.id.main_content);
-            Log.i(TAG, "Current Fragment is : " + currentFragment);
             FragmentTransaction fragmentTransaction = manager.beginTransaction();
             fragmentTransaction.detach(currentFragment);
             fragmentTransaction.attach(currentFragment);
             fragmentTransaction.commit();
             manager.executePendingTransactions();
+            setPreferenceScreen(getPreferenceScreen());
         } catch (Exception e) {
+            Log.e(TAG, "WTF......" + e);
         }
-        setTextColorSummary();
+        setSummaries();
     }
 
     @Override
